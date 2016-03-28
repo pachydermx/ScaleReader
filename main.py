d3a466e5cd7c9b09b5d9131e2797b694cce688e0
Ren Jimin <renjimin@gmail.com>
2016-03-28 14:58:21 +0900

    Find Screen Border

---
+++ main.py
@@ -0,0 +1,32 @@
+from pyimagesearch import imutils
+from skimage import exposure
+import numpy as np
+import argparse
+import cv2
+
+image = cv2.imread("test.jpg")
+ratio = image.shape[0] / 600.0
+orig = image.copy()
+image = imutils.resize(image, height = 600)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.bilateralFilter(gray, 11, 17, 17)
+edged = cv2.Canny(gray, 30, 200)
+
+cv2.imshow("Edged", edged)
+
+_, cnts, _ = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
+screenCnt = None
+
+for c in cnts:
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+    if len(approx) == 4:
+        screenCnt = approx
+        break
+
+cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 3)
+cv2.imshow("Gameboy screen", image)
+cv2.waitKey(0)
