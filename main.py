029f8d75da1c3214be75127eda66dd8c21187fe7
Ren Jimin <renjimin@gmail.com>
2016-03-28 15:16:25 +0900

    Perspective Crop

--- main.py
+++ main.py
@@ -29,4 +29,42 @@ for c in cnts:
 
 cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 3)
 cv2.imshow("Gameboy screen", image)
+
+pts = screenCnt.reshape(4, 2)
+rect = np.zeros((4, 2), dtype = "float32")
+
+s = pts.sum(axis = 1)
+rect[0] = pts[np.argmin(s)]
+rect[2] = pts[np.argmax(s)]
+
+diff = np.diff(pts, axis = 1)
+rect[1] = pts[np.argmin(diff)]
+rect[3] = pts[np.argmax(diff)]
+
+rect *= ratio
+
+(tl, tr, br, bl) = rect
+widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+
+heightA = np.sqrt(((tr[0] - [br[0]]) ** 2) + ((tr[1] - bl[1]) ** 2))
+heightB = np.sqrt(((tl[0] - [bl[0]]) ** 2) + ((tl[1] - bl[1]) ** 2))
+
+maxWidth = max(int(widthA), int(widthB))
+maxHeight = max(int(heightA), int(heightB))
+
+dst = np.array([
+    [0, 0],
+    [maxWidth - 1, 0],
+    [maxWidth - 1, maxHeight - 1],
+    [0, maxHeight - 1]
+], dtype = "float32")
+
+M = cv2.getPerspectiveTransform(rect, dst)
+warp = cv2.warpPerspective(orig, M, (maxWidth, maxHeight))
+
+cv2.imshow("Warp", warp)
+
+# wait
 cv2.waitKey(0)
+
