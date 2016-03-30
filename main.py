95987cc47ea595bd0bd1c751a9ab105d2cb2edc3
Ren Jimin <renjimin@gmail.com>
2016-03-30 11:49:43 +0900

    Load Video

--- main.py
+++ main.py
@@ -5,6 +5,29 @@ import cv2
 from numberrec import NumberRecognizer
 
 class ScaleReader:
+
+    def getImageFromCamera(self, id):
+        cap = cv2.VideoCapture(id)
+        while True:
+            ret, frame = cap.read()
+            frame = cv2.resize(frame, (800, 600))
+            cv2.imshow("Camera", frame)
+
+            self.init_image(frame)
+
+            k = cv2.waitKey(1)
+
+    def getImageFromVideo(self):
+        cap = cv2.VideoCapture('test.avi')
+        while cap.isOpened():
+            ret, frame = cap.read()
+            frame = cv2.resize(frame, (720, 480))
+            cv2.imshow("Video Input", frame)
+
+            self.init_image(frame)
+
+            k = cv2.waitKey(1)
+
     def onMouseClick(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
             self.start = [x, y]
@@ -12,19 +35,19 @@ class ScaleReader:
         if event == cv2.EVENT_LBUTTONUP:
             self.end = [x, y]
             test =self.recognize_block(self.start[0], self.end[0], self.start[1], self.end[1])
+            print "[" , self.start[0] , "," , self.end[0], ",", self.start[1], ",", self.end[1] , "], "
             #print test
 
-    def init_image(self):
-        image = cv2.imread("test.jpg")
+    def init_image(self, image):
         ratio = image.shape[0] / 600.0
         orig = image.copy()
-        image = imutils.resize(image, height = 600)
+        #image = imutils.resize(image, height = 600)
 
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         gray = cv2.bilateralFilter(gray, 11, 17, 17)
         edged = cv2.Canny(gray, 30, 200)
 
-        #cv2.imshow("Edged", edged)
+        cv2.imshow("Edged", edged)
 
         _, cnts, _ = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
@@ -38,59 +61,60 @@ class ScaleReader:
                 screenCnt = approx
                 break
 
-        cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 3)
-        #cv2.imshow("Gameboy screen", image)
+        if screenCnt != None and False:
+            cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 3)
+            #cv2.imshow("Gameboy screen", image)
 
-        pts = screenCnt.reshape(4, 2)
-        rect = np.zeros((4, 2), dtype = "float32")
+            pts = screenCnt.reshape(4, 2)
+            rect = np.zeros((4, 2), dtype = "float32")
 
-        s = pts.sum(axis = 1)
-        rect[0] = pts[np.argmin(s)]
-        rect[2] = pts[np.argmax(s)]
+            s = pts.sum(axis = 1)
+            rect[0] = pts[np.argmin(s)]
+            rect[2] = pts[np.argmax(s)]
 
-        diff = np.diff(pts, axis = 1)
-        rect[1] = pts[np.argmin(diff)]
-        rect[3] = pts[np.argmax(diff)]
+            diff = np.diff(pts, axis = 1)
+            rect[1] = pts[np.argmin(diff)]
+            rect[3] = pts[np.argmax(diff)]
 
-        rect *= ratio
+            rect *= ratio
 
-        (tl, tr, br, bl) = rect
-        widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-        widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+            (tl, tr, br, bl) = rect
+            widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+            widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
 
-        heightA = np.sqrt(((tr[0] - [br[0]]) ** 2) + ((tr[1] - bl[1]) ** 2))
-        heightB = np.sqrt(((tl[0] - [bl[0]]) ** 2) + ((tl[1] - bl[1]) ** 2))
+            heightA = np.sqrt(((tr[0] - [br[0]]) ** 2) + ((tr[1] - bl[1]) ** 2))
+            heightB = np.sqrt(((tl[0] - [bl[0]]) ** 2) + ((tl[1] - bl[1]) ** 2))
 
-        maxWidth = max(int(widthA), int(widthB))
-        maxHeight = max(int(heightA), int(heightB))
+            maxWidth = max(int(widthA), int(widthB))
+            maxHeight = max(int(heightA), int(heightB))
 
-        dst = np.array([
-            [0, 0],
-            [maxWidth - 1, 0],
-            [maxWidth - 1, maxHeight - 1],
-            [0, maxHeight - 1]
-        ], dtype = "float32")
+            dst = np.array([
+                [0, 0],
+                [maxWidth - 1, 0],
+                [maxWidth - 1, maxHeight - 1],
+                [0, maxHeight - 1]
+            ], dtype = "float32")
 
-        M = cv2.getPerspectiveTransform(rect, dst)
-        warp = cv2.warpPerspective(orig, M, (maxWidth, maxHeight))
+            M = cv2.getPerspectiveTransform(rect, dst)
+            warp = cv2.warpPerspective(orig, M, (maxWidth, maxHeight))
 
-        #cv2.imshow("Warp", warp)
+            #cv2.imshow("Warp", warp)
 
-        gray = cv2.cvtColor(warp, cv2.COLOR_BGR2GRAY)
-        gray = cv2.bilateralFilter(gray, 17, 17, 17)
+            gray = cv2.cvtColor(warp, cv2.COLOR_BGR2GRAY)
+            gray = cv2.bilateralFilter(gray, 17, 17, 17)
 
-        athresholded = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-        ret, thresholded = cv2.threshold(gray,60, 255, cv2.THRESH_BINARY)
-        cv2.namedWindow("Grayscale")
-        cv2.setMouseCallback("Grayscale", self.onMouseClick)
+            athresholded = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+            ret, thresholded = cv2.threshold(gray,60, 255, cv2.THRESH_BINARY)
+            cv2.namedWindow("Grayscale")
+            cv2.setMouseCallback("Grayscale", self.onMouseClick)
 
-        self.athrimg = athresholded.copy()
-        self.thrimg = thresholded.copy()
-        cv2.imshow("Grayscale", self.athrimg)
-        #cv2.imshow("THRES", self.thrimg)
+            self.athrimg = athresholded.copy()
+            self.thrimg = thresholded.copy()
+            cv2.imshow("Grayscale", self.athrimg)
+            #cv2.imshow("THRES", self.thrimg)
 
-        # wait
-        #cv2.waitKey(0)
+            # wait
+            #cv2.waitKey(0)
 
     def recognize_block(self, args):
         x0 = args[0]
@@ -102,7 +126,6 @@ class ScaleReader:
         mean_ath, _, _, _ = cv2.mean(cropped_img_ath)
         mean_th, _, _, _ = cv2.mean(cropped_img_th)
         mean = (mean_ath + mean_th) / 2
-        #print "[" , self.start[0] , "," , self.end[0], ",", self.start[1], ",", self.end[1] , "], "
         if mean < 200:
             return True
         else:
@@ -129,7 +152,6 @@ class ScaleReader:
         result /= 10
         print result
 
-
     def init_frame(self):
         self.number_frame = [
             [
@@ -152,9 +174,12 @@ class ScaleReader:
         ]
 
     def __init__(self):
+        image = cv2.imread("test.jpg")
         self.init_frame()
-        self.init_image()
-        self.recognize_number(self.number_frame)
+        self.getImageFromVideo()
+        #self.getImageFromCamera(0)
+        #self.init_image(image)
+        #self.recognize_number(self.number_frame)
 
 
 sr = ScaleReader()
